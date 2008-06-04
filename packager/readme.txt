cbfbe03e676444e3a1320874396853d9d4e517da
Colin Willcocks <gumtownbassman@yahoo.com>
2008-06-04 11:38:32 +0000

    GT8_2_GT10 patch conversion

--- packager/readme.txt
+++ packager/readme.txt
@@ -1,43 +1,40 @@
-*********************GT-FxFloorBoard Preview version: 27 may 2008/revision B*************************
+*********************GT-FxFloorBoard Preview version: 05 June 2008 *************************
 
 ****NOTE: THIS IS A PREVIEW VERSION AND MANY FEATURES HAVE NOT YET BEEN IMPLIMENTED, OR SOME FUNCTIONS MAY BE INCORRECT.**** 
 
-yet to sort chain structure concept. chain will currently place all FX into channel A path.
+a.)  yet to sort chain structure concept. chain will currently place all FX into channel A path.
 try out all the pedals by double or right click on stomp body area, let me know which parameters are incorrect.
 
-saved files will not produce %100 GT10 accurate files yet.
-if chain items are moved/dragged, all FX items end up Path A only.
-recieved patch data from GT-10 is abled to be saved to file with correct chain data in syx format.
+b.)  Any editing done with the 'Chain Order' will result in all fX to Path "A" only.
+recieved patch data from GT-10 is able to be saved to file with correct chain data in syx format.
 saved files can be re-loaded in to the Editor and passed to the GT-10
 
-layout increased to fit 18 chain events, may be a problem for low screen resolutions. (i use 1280 X 800)
+c.)  layout increased to fit 18 chain events, may be a problem for low screen resolutions. (i use 1280 X 800)
 does this fit on your screen?
 
-continuing sessions at converting from GT-8 version most items are listed,
+d.)  continuing sessions at converting from GT-8 version most items are listed,
 with exception of assigns and custom user settings for autoriff & harmonizer.
 
-patch tree to the left side should give patch change on single click and (when 'connected' button active),
+e.)  patch tree to the left side should give patch change on single click and (when 'connected' button active),
  double clicking the tree patch item will request a patch transfer from the GT-10.
 opening each patch tree bank node  will request patch names (while 'connected'). 
-does this work for user & preset patches?
 
-patch rename by doubleclicking name display panel.
+f.)  patch rename by doubleclicking name display panel.
 
-out-going midi & patch data should be recognized by GT-10. 
+g.)  out-going midi & patch data will be recognized by GT-10. 
 Editor internal structure now ready to receive a patch.
 
-Any editing done with the 'Chain Order' will result in all fX to Path "A".
+h.)  GT-8 patch conversion. Gt-8 patches can be opened (%50 completed)
 
 
 
 ****my wish list (things i would like to know, if it works for you): 
-		***find temp buffer individual write address,
- 		***the bulk address may not be suitable for faster patch tweaking rates.
 
-		***user patch change works correctly with one single click of patch tree item in User and Preset patches.
+		1.)  ***find temp buffer individual write address,
+ 		     ***the bulk address may not be suitable for faster patch tweaking rates.
 
-		***check realtime edit parameters are correct address and correct data range (is the editor tweaking the right GT-10 item).
+		2.)  ***check realtime edit parameters are correct address and correct data range (is the editor tweaking the right GT-10 item).
 
-
+		3.)  ***check chain positions are correct.
 
 reply email to <gumtownbassman@yahoo.com> as your information will help with the project progress.
--- sysxWriter.cpp
+++ sysxWriter.cpp
@@ -50,16 +50,15 @@ bool sysxWriter::readFile()
 	QFile file(fileName);
     if (file.open(QIODevice::ReadOnly))
 	{
-		QByteArray data = file.readAll();
+		QByteArray data = file.readAll();     // read the pre-selected file, copy to 'data'
 
-		if(data.size() == patchSize){         // if patch file size is correct- load file.
+		if(data.size() == patchSize){         // if GT-10 patch file size is correct- load file.
 		SysxIO *sysxIO = SysxIO::Instance();
 		sysxIO->setFileSource(data);
 		sysxIO->setFileName(this->fileName);
-
 		this->fileSource = sysxIO->getFileSource();
 		return true;
-		}	else if (data.size() == 1010){
+		}	else if (data.size() == 1010){      // if the file size = gt-8 patch
 		
 	QMessageBox *msgBox = new QMessageBox();
 	msgBox->setWindowTitle(QObject::tr("Patch file conversion"));
@@ -78,50 +77,113 @@ bool sysxWriter::readFile()
 	QFile file(":default.syx");   // Read the default GT-10 sysx file so whe don't start empty handed.
     if (file.open(QIODevice::ReadOnly))
 	{	data = file.readAll(); };
-	QByteArray temp;                           // TRANSLATION of GT-8 PATCHES **************
-	QByteArray x = "00";
-	 temp = gt8_data.mid(692, 16);            // copy gt8 name
+	QByteArray temp;                         // TRANSLATION of GT-8 PATCHES, data read from gt8 patch **************
+	QByteArray x;                            // and used to replace gt10 patch data*********************************
+	 temp = gt8_data.mid(692, 16);           // copy gt8 name
 	data.replace(11, 16, temp);              // replace gt10 name
 	temp = gt8_data.mid(154, 1);             // copy gt8 compressor part1
 	temp.append(gt8_data.mid(156,7));        // copy gt8 compressor part2
 	data.replace(75, 8, temp);               // replace gt10 compressor
 	temp = gt8_data.mid(212, 1);             // copy gt8 dist/od part1
 	temp.append(gt8_data.mid(214,5));        // copy gt8 dist/od part2
-	data.replace(123, 6, temp);               // replace gt10 distortion
-	// adjust dist types here
+	data.replace(123, 6, temp);              // replace gt10 distortion
+	// todo adjust dist types here
 	temp = gt8_data.mid(233, 1);             // copy gt8 preAmp control part1
 	temp.append(gt8_data.mid(235,2));        // copy gt8 preAmp control part2
 	temp.append(gt8_data.mid(238,1));        // copy gt8 preAmp control part3
 	temp.append(gt8_data.mid(237,1));        // copy gt8 preAmp control part4
-	data.replace(152, 5, temp);               // replace gt10 preAmp control
-	temp = gt8_data.mid(239, 17);             // copy gt8 preAmp channel A 
-	data.replace(168, 17, temp);               // replace gt10 preAmp channel A
-	temp = gt8_data.mid(256, 17);             // copy gt8 preAmp channel B 
-	data.replace(200, 17, temp);               // replace gt10 preAmp channel B
+	data.replace(152, 5, temp);              // replace gt10 preAmp control
+	temp = gt8_data.mid(239, 17);            // copy gt8 preAmp channel A 
+	data.replace(168, 17, temp);             // replace gt10 preAmp channel A
+	// todo adjust preamp types
+	temp = gt8_data.mid(256, 17);            // copy gt8 preAmp channel B 
+	data.replace(200, 17, temp);             // replace gt10 preAmp channel B
+	// todo adjust preamp types
 	temp = gt8_data.mid(286, 1);             // copy gt8 EQ part1
 	temp.append(gt8_data.mid(288,11));       // copy gt8 EQ part2
-	data.replace(264, 12, temp);               // replace gt10 EQ
+	data.replace(264, 12, temp);             // replace gt10 EQ
 	
 	
-	temp = gt8_data.mid(545, 1);             // copy gt8 delay part1
-	temp.append(gt8_data.mid(547,16));       // copy gt8 delay part2
+	temp = gt8_data.mid(545, 1);            // copy gt8 delay part1
+	temp.append(gt8_data.mid(547,16));      // copy gt8 delay part2
 	temp.append(gt8_data.mid(565,4));       // copy gt8 delay part3
 	temp.append(gt8_data.mid(563,2));       // copy gt8 delay part4
 	temp.append(gt8_data.mid(569,2));       // copy gt8 delay part5
-	if (temp.mid(2,1)=="11"){temp.replace(2, 1, "00");};
-	data.replace(1351, 25, temp);               // replace gt10 delay
-	temp = gt8_data.mid(584, 1);             // copy gt8 chorus part1
+	if (temp.mid(2,1)=="11"){temp.replace(2, 1, "00"); };
+	data.replace(1351, 25, temp);           // replace gt10 delay
+	temp = gt8_data.mid(584, 1);            // copy gt8 chorus part1
 	temp.append(gt8_data.mid(586,7));       // copy gt8 chorus part2
-	data.replace(1383, 8, temp);               // replace gt10 chorus
-	temp = gt8_data.mid(606, 1);             // copy gt8 reverb part1
+	data.replace(1383, 8, temp);            // replace gt10 chorus
+	temp = gt8_data.mid(606, 1);            // copy gt8 reverb part1
 	temp.append(gt8_data.mid(608,3));       // copy gt8 reverb part2
 	temp.append(gt8_data.mid(611,5));       // copy gt8 reverb part3
-	data.replace(1399, 9, temp);               // replace gt10 reverb
-	temp = (gt8_data.mid(610,1));              // copy gt8 reverb part4
-	data.replace(1410, 1, temp);             // replace gt10 reverb
+	data.replace(1399, 9, temp);            // replace gt10 reverb
+	temp = (gt8_data.mid(610,1));           // copy gt8 reverb part4
+	data.replace(1410, 1, temp);            // replace gt10 reverb
+	
+	temp = (gt8_data.mid(665,14));          // copy gt8_chain
+	 int a = temp.indexOf(data.mid(1506,1)); // locate gt10_preamp 1 **** depends on default.syx
+	temp.insert(a, data.mid(1507,1));          // insert gt10_merge after preamp 1
+	temp.insert(a-1, data.mid(1495,1));          //insert gt10_split before preamp 1
+	temp.insert(a, data.mid(1504,1));
+	temp.insert(a, data.mid(1505,1));
+	
+	QString z;
+	QString y;
+	/*for (int i=0; i<18; i++){
+	y = (QString::number(i, 10));
+	if (y.size()<2) {y.prepend("0");}; 
+  a = temp.indexOf(data.mid(1492,1));  temp.replace(a, 1, data.mid(1503,1));   // 00 to 05
+  a = temp.indexOf(data.mid(1493,1));  temp.replace(a, 1, data.mid(1492,1));   // 01 to 00
+  a = temp.indexOf(data.mid(1508,1));  temp.replace(a, 1, data.mid(1500,1));   // 02 to 0A
+   if (temp.mid(i,1)=="03"){temp.replace(i, 1, "0E");  }      // 03 to 0E  todo
+  else if (temp.mid(i,1)=="04"){temp.replace(i, 1, "01");  }     // 04 to 01
+  else if (temp.mid(i,1)=="05"){temp.replace(i, 1, "02");  }
+  else if (temp.mid(i,1)=="06"){temp.replace(i, 1, "04");  }
+  else if (temp.mid(i,1)=="07"){temp.replace(i, 1, "06");  }
+  else if (temp.mid(i,1)=="08"){temp.replace(i, 1, "07");  }
+  else if (temp.mid(i,1)=="09"){temp.replace(i, 1, "08");  }
+  else if (temp.mid(i,1)=="0A"){temp.replace(i, 1, "09");  }
+  else if (temp.mid(i,1)=="0B"){temp.replace(i, 1, "0C");  }
+  else if (temp.mid(i,1)=="0C"){temp.replace(i, 1, "0B");  }
+  else if (temp.mid(i,1)=="0D"){temp.replace(i, 1, "0F");  };
+  z.append(y);*/
+  //};  
+	data.replace(1492, 18, temp);             // replace gt10 chain
 	
 	
 	
+	/*
+	
+	 QString snork;
+			snork.append("<font size='-1'>");
+			snork.append(" { size=");
+			snork.append(QString::number(temp.size(), 10));
+			snork.append("}");	
+			
+			snork.append(" { size z=");
+			snork.append(QString::number(z.size()/2, 10));
+			snork.append("}");	
+				snork.append("<br> midi data received  ");
+			for(int i=0;i<temp.size();++i)
+			{
+				snork.append(temp.mid(i, 2));
+				snork.append(" ");
+				//i++;
+			};
+			snork.append("<br> midi data received  ");
+			for(int i=0;i<z.size();++i)
+			{
+				snork.append(z.mid(i*2, 2));
+				snork.append(" ");
+				//i++;
+			};
+			QMessageBox *msgBox = new QMessageBox();
+			msgBox->setWindowTitle("dBug Result");
+			msgBox->setIcon(QMessageBox::Information);
+			msgBox->setText(snork);
+			msgBox->setStandardButtons(QMessageBox::Ok);
+			msgBox->exec();*/
 	
     SysxIO *sysxIO = SysxIO::Instance();
 		sysxIO->setFileSource(data);
